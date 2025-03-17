d0e8dc3a48bcc483270293bedd36de9e5dfb2c74
Dani3lLopez <danlop112004@gmail.com>
2025-03-16 23:23:43 -0600

    CRUD(Consultas)

--- src/Controllers/ConsultationsController.java
+++ src/Controllers/ConsultationsController.java
@@ -1,4 +1,195 @@
 package src.Controllers;
 
-public class ConsultationsController {
+import src.Models.ConsultationsModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultationsController extends DoctorsController{
+    public ConsultationsController() {
+        super();
+        listaConsultas = new ArrayList<>();
+        listaMascotas = new ArrayList<>();
+        listaDoctores = new ArrayList<>();
+    }
+
+    private String idConsulta;
+    private String fechaConsulta;
+    private String motivoConsulta;
+    private String diagnosticoConsulta;
+    private String notasConsulta;
+    private String idMascota;
+    private String idDoctor;
+    private Boolean visibilidadConsulta;
+    private List<List<String>> listaConsultas;
+    private List<List<String>> listaMascotas;
+    private List<List<String>> listaDoctores;
+
+    public String getIdConsulta() {
+        return idConsulta;
+    }
+
+    public void setIdConsulta(String idConsulta) {
+        this.idConsulta = idConsulta;
+    }
+
+    public String getFechaConsulta() {
+        return fechaConsulta;
+    }
+
+    public void setFechaConsulta(String fechaConsulta) {
+        this.fechaConsulta = fechaConsulta;
+    }
+
+    public String getMotivoConsulta() {
+        return motivoConsulta;
+    }
+
+    public void setMotivoConsulta(String motivoConsulta) {
+        this.motivoConsulta = motivoConsulta;
+    }
+
+    public String getDiagnosticoConsulta() {
+        return diagnosticoConsulta;
+    }
+
+    public void setDiagnosticoConsulta(String diagnosticoConsulta) {
+        this.diagnosticoConsulta = diagnosticoConsulta;
+    }
+
+    public String getNotasConsulta() {
+        return notasConsulta;
+    }
+
+    public void setNotasConsulta(String notasConsulta) {
+        this.notasConsulta = notasConsulta;
+    }
+
+    public String getIdMascota() {
+        return idMascota;
+    }
+
+    public void setIdMascota(String idMascota) {
+        this.idMascota = idMascota;
+    }
+
+    @Override
+    public String getIdDoctor() {
+        return idDoctor;
+    }
+
+    @Override
+    public void setIdDoctor(String idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public Boolean getVisibilidadConsulta() {
+        return visibilidadConsulta;
+    }
+
+    public void setVisibilidadConsulta(Boolean visibilidadConsulta) {
+        this.visibilidadConsulta = visibilidadConsulta;
+    }
+
+    public List<List<String>> listaConsultas() {
+        return listaConsultas;
+    }
+
+    public List<List<String>> listaMascotas() {
+        return listaMascotas;
+    }
+
+    public List<List<String>> listaDoctores() {
+        return listaDoctores;
+    }
+
+    public void llenarListas(){
+        listaConsultas = ConsultationsModel.cargarListaConsultas();
+        listaMascotas = ConsultationsModel.cargarListaMascotas();
+        listaDoctores = ConsultationsModel.cargarListaDoctores();
+        cargarListaPersonas();
+    }
+
+    public String capturarIdLista(int numero) {
+        if (numero > 0 && numero <= listaConsultas.size()) {
+            return listaConsultas.get(numero - 1).get(0);
+        }
+        return null;
+    }
+
+    public String capturarIdListaMascotas(int numero) {
+        if (numero > 0 && numero <= listaMascotas.size()) {
+            return listaMascotas.get(numero - 1).get(0);
+        }
+        return null;
+    }
+
+    public String capturarIdListaDoctores(int numero) {
+        if (numero > 0 && numero <= listaDoctores.size()) {
+            return listaDoctores.get(numero - 1).get(0);
+        }
+        return null;
+    }
+
+    public String capturarNombresDoctores(String idPersona) {
+        for (List<String> doctor : listaDoctores()) {
+            if (doctor.get(3).trim().equalsIgnoreCase(idPersona.trim())) {
+                for (List<String> persona : listaPersonas()) {
+                    if (persona.get(0).trim().equalsIgnoreCase(idPersona.trim())) {
+                        return persona.get(1) + " " + persona.get(2);
+                    }
+                }
+            }
+        }
+        return "Desconocido";
+    }
+
+    public String capturarMascotas(String idMascota){
+        for (List<String> mascota : listaMascotas()) {
+            if (mascota.get(0).trim().equalsIgnoreCase(idMascota)) {
+                return mascota.get(1);
+            }
+        }
+        return "No encontrada";
+    }
+
+    public int registrarConsulta() {
+        return ConsultationsModel.ingresarNuevaConsulta(fechaConsulta, motivoConsulta, diagnosticoConsulta, notasConsulta, idMascota, idDoctor, visibilidadConsulta);
+    }
+
+    public List<String> cargarDatosConsulta(int registro){
+        String id = capturarIdLista(registro);
+        if (id != null){
+            return AppointmentsModel.cargarCita(id);
+        }
+        return new ArrayList<>();
+    }
+
+    public void actualizarConsulta(int registro, String fechaConsulta, String motivoConsulta, String diagnosticoConsulta, String notasConsulta, String idMascota, String idDoctor, Boolean visibilidadConsulta) {
+        String id = capturarIdLista(registro);
+        if (id != null) {
+            int resultado = ConsultationsModel.actualizarConsulta(id, fechaConsulta, motivoConsulta, diagnosticoConsulta, notasConsulta, idMascota, idDoctor, visibilidadConsulta);
+            if (resultado > 0) {
+                System.out.println("Registro actualizado correctamente.");
+            } else {
+                System.out.println("Error al actualizar el registro.");
+            }
+        } else {
+            System.out.println("Registro inexistente.");
+        }
+    }
+
+    public void desactivarConsulta(int numero){
+        String id = this.capturarIdLista(numero);
+        if (id != null) {
+            int resultado = ConsultationsModel.desactivarConsulta(id);
+            if (resultado > 0) {
+                System.out.println("Consulta desactivada correctamente.");
+            } else {
+                System.out.println("Error al desactivar el registro.");
+            }
+        } else {
+            System.out.println("Registro inexistente.");
+        }
+    }
 }
--- src/Models/ConsultationsModel.java
+++ src/Models/ConsultationsModel.java
@@ -1,4 +1,172 @@
 package src.Models;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsultationsModel {
+    public static List<List<String>> cargarListaConsultas() {
+        List<List<String>> listaConsultas = new ArrayList<>();
+        String sql = "SELECT * FROM consultas";
+
+        try (
+                Connection conexion = ConnectionModel.conectar();
+                PreparedStatement ps = conexion.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+        ) {
+            while (rs.next()) {
+                List<String> consulta = new ArrayList<>();
+                consulta.add(rs.getString("id_consulta"));
+                consulta.add(rs.getString("fecha_consulta"));
+                consulta.add(rs.getString("motivo_consulta"));
+                consulta.add(rs.getString("diagnostico_consulta"));
+                consulta.add(rs.getString("notas_consulta"));
+                consulta.add(rs.getString("id_mascota"));
+                consulta.add(rs.getString("id_doctor"));
+                consulta.add(rs.getString("visibilidad_consulta"));
+
+                listaConsultas.add(consulta);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al leer datos: " + e.getMessage());
+        }
+
+        return listaConsultas;
+    }
+    public static List<List<String>> cargarListaMascotas() {
+        List<List<String>> listaMascotas = new ArrayList<>();
+        String sql = "SELECT id_mascota, nombre_mascota, color_mascota, peso_mascota, unidad_peso_mascota, genero_mascota, codigo_chip_mascota, estado_reproductivo_mascota, fecha_nacimiento_mascota, talla_mascota, fallecimiento_mascota, razon_fallecimiento FROM mascotas WHERE visibilidad_mascota = 1";
+
+        try (
+                Connection conexion = ConnectionModel.conectar();
+                PreparedStatement ps = conexion.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+        ) {
+            while (rs.next()) {
+                List<String> mascota = new ArrayList<>();
+                mascota.add(rs.getString("id_mascota"));
+                mascota.add(rs.getString("nombre_mascota"));
+                mascota.add(rs.getString("color_mascota"));
+                mascota.add(rs.getString("peso_mascota"));
+                mascota.add(rs.getString("unidad_peso_mascota"));
+                mascota.add(rs.getString("genero_mascota"));
+                mascota.add(rs.getString("codigo_chip_mascota"));
+                mascota.add(rs.getString("estado_reproductivo_mascota"));
+                mascota.add(rs.getString("fecha_nacimiento_mascota"));
+                mascota.add(rs.getString("talla_mascota"));
+                mascota.add(rs.getString("fallecimiento_mascota"));
+                mascota.add(rs.getString("razon_fallecimiento"));
+
+                listaMascotas.add(mascota);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al leer datos: " + e.getMessage());
+        }
+
+        return listaMascotas;
+    }
+    public static List<List<String>> cargarListaDoctores() {
+        List<List<String>> listaDoctores = new ArrayList<>();
+        String sql = "SELECT * FROM doctores";
+
+        try (
+                Connection conexion = ConnectionModel.conectar();
+                PreparedStatement ps = conexion.prepareStatement(sql);
+                ResultSet rs = ps.executeQuery()
+        ) {
+            while (rs.next()) {
+                List<String> doctor = new ArrayList<>();
+                doctor.add(rs.getString("id_doctor"));
+                doctor.add(rs.getString("fecha_contratacion_doctor"));
+                doctor.add(rs.getString("fecha_nacimiento_doctor"));
+                doctor.add(rs.getString("id_persona"));
+                doctor.add(rs.getString("id_especialidad"));
+
+                listaDoctores.add(doctor);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al leer datos: " + e.getMessage());
+        }
+
+        return listaDoctores;
+    }
+    public static List<String> cargarCita(String id){
+        List<String> datosCita = new ArrayList<>();
+        String sql = "SELECT * FROM citas WHERE id_cita = ?";
+
+        try (
+                Connection conexion = ConnectionModel.conectar();
+                PreparedStatement ps = conexion.prepareStatement(sql);
+        ) {
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                datosCita.add(rs.getString("id_cita"));
+                datosCita.add(rs.getString("motivo_cita"));
+                datosCita.add(rs.getString("fecha_cita"));
+                datosCita.add(rs.getString("hora_cita"));
+                datosCita.add(rs.getString("id_mascota"));
+                datosCita.add(rs.getString("id_doctor"));
+                datosCita.add(rs.getString("visibilidad_cita"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al leer datos: " + e.getMessage());
+        }
+
+        return datosCita;
+    }
+    public static int ingresarNuevaConsulta(String fechaConsulta, String motivoConsulta, String diagnosticoConsulta, String notasConsulta, String idMascota, String idDoctor, Boolean visibilidadConsulta){
+        int retorno = 0;
+        String sql = "INSERT INTO consultas (fecha_consulta, motivo_consulta, diagnostico_consulta, notas_consulta, id_mascota, id_doctor, visibilidad_consulta) VALUES (?,?,?,?,?,?,?)";
+        try(
+                Connection conexion = ConnectionModel.conectar();
+                PreparedStatement ps = conexion.prepareStatement(sql)){
+            ps.setString(1, fechaConsulta);
+            ps.setString(2, motivoConsulta);
+            ps.setString(3, diagnosticoConsulta);
+            ps.setString(4, notasConsulta);
+            ps.setString(5, idMascota);
+            ps.setString(6, idDoctor);
+            ps.setBoolean(7, visibilidadConsulta);
+
+            retorno = ps.executeUpdate();
+            return retorno;
+        } catch (SQLException e) {
+            System.out.println("Error al registrar datos: " + e.getMessage());
+            return retorno;
+        }
+    }
+    public static int actualizarConsulta(String id, String fechaConsulta, String motivoConsulta, String diagnosticoConsulta, String notasConsulta, String idMascota, String idDoctor, Boolean visibilidadConsulta) {
+        String sql = "UPDATE consultas SET fecha_consulta=?, motivo_consulta=?, diagnostico_consulta=?, notas_consulta=?, id_mascota=?, id_doctor=?, visibilidad_consulta=? WHERE id_consulta=?";
+        try (Connection conexion = ConnectionModel.conectar();
+             PreparedStatement ps = conexion.prepareStatement(sql)) {
+            ps.setString(1, fechaConsulta);
+            ps.setString(2, motivoConsulta);
+            ps.setString(3, diagnosticoConsulta);
+            ps.setString(4, notasConsulta);
+            ps.setString(5, idMascota);
+            ps.setString(6, idDoctor);
+            ps.setBoolean(7, visibilidadConsulta);
+            ps.setString(8, id);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error al actualizar consulta: " + e.getMessage());
+        }
+        return 0;
+    }
+    public static int desactivarConsulta(String id) {
+        String sql = "UPDATE citas SET visibilidad_cita = 0 WHERE id_cita=?";
+        try (Connection conexion = ConnectionModel.conectar();
+             PreparedStatement ps = conexion.prepareStatement(sql)) {
+            ps.setString(1, id);
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error al cambiar visibilidad del registro: " + e.getMessage());
+        }
+        return 0;
+    }
 }
