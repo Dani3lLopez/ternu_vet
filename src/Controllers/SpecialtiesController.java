3c6ac42db8323189b3a13fe44bfc906fd8924990
belsusaan <susanabeltran679@gmail.com>
2025-03-19 14:17:46 -0600

    Update SpecialtiesController.java

--- src/Controllers/SpecialtiesController.java
+++ src/Controllers/SpecialtiesController.java
@@ -10,14 +10,16 @@ public class SpecialtiesController {
     public SpecialtiesController() {
     }
 
+    // Creamos atributos privados para poder almacenar los datos de una especialidad
     private String idEspecialidad;
     private String nombreEspecialidad;
-    private List<List<String>> listaEspecialidades;
+    private List<List<String>> listaEspecialidades; // En esta lista de listas se almacenaran dichos datos
 
     public String getIdEspecialidad() {
         return idEspecialidad;
     }
 
+    // Getters y settters creados para cada uno de los atributos
     public void setIdEspecialidad(String idEspecialidad) {
         this.idEspecialidad = idEspecialidad;
     }
@@ -30,22 +32,31 @@ public class SpecialtiesController {
         this.nombreEspecialidad = nombreEspecialidad;
     }
 
+    // Dado un número de registro, se captura el ID de una especialidad
     public String capturarIdLista(int numero) {
         if (numero > 0 && numero <= listaEspecialidades.size()) {
             return listaEspecialidades.get(numero - 1).get(0);
         }
         return null;
     }
+
+    // Desde el modelo, carga la lista de especialidades
     public void cargarListaEspecialidades() {
         listaEspecialidades = SpecialtiesModel.cargarListaEspecialidades();
     }
+
+    // Muestra la lista cargada
     public List<List<String>> listaEspecialidades() {
         return listaEspecialidades;
     }
+
+    // Registra la nueva especialidad
     public int RegistrarEspecialidad() {
         return SpecialtiesModel.ingresarNuevaEspecialidad(nombreEspecialidad);
     }
-    public void eliminarEspecialidad(int numero){
+
+    // Dado un número de registro, se elemina una especialidad
+    public void eliminarEspecialidad(int numero) {
         String id = this.capturarIdLista(numero);
         if (id != null) {
             int resultado = SpecialtiesModel.eliminarEspecialidad(id);
