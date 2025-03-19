34bb96ec1dc4a9f71b300acb21e446663093f8a0
belsusaan <susanabeltran679@gmail.com>
2025-03-19 14:20:35 -0600

    Update ServicesModel.java

--- src/Models/ServicesModel.java
+++ src/Models/ServicesModel.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+// carga la lista de servicios
 public class ServicesModel {
     public static List<List<String>> cargarListaServicios() {
         List<List<String>> listaServicios = new ArrayList<>();
@@ -15,8 +16,7 @@ public class ServicesModel {
         try (
                 Connection conexion = ConnectionModel.conectar();
                 PreparedStatement ps = conexion.prepareStatement(sql);
-                ResultSet rs = ps.executeQuery()
-        ) {
+                ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 List<String> servicio = new ArrayList<>();
                 servicio.add(rs.getString("id_servicio"));
@@ -27,18 +27,19 @@ public class ServicesModel {
                 listaServicios.add(servicio);
             }
         } catch (SQLException e) {
-            System.out.println("Error al leer datos: " + e.getMessage());
+            System.out.println("Error al leer datos: " + e.getMessage()); // Si lo anterior da error, se muestra esto
         }
 
         return listaServicios;
     }
 
+    // Para agregar un nuevo servicio
     public static int ingresarServicio(String nombreServicio, String descripcionServicio, double precioServicio) {
         int retorno = 0;
         String sql = "INSERT INTO servicios (nombre_servicio, descripcion_servicio, precio_servicio) VALUES (?,?,?)";
-        try(
+        try (
                 Connection conexion = ConnectionModel.conectar();
-                PreparedStatement ps = conexion.prepareStatement(sql)){
+                PreparedStatement ps = conexion.prepareStatement(sql)) {
             ps.setString(1, nombreServicio);
             ps.setString(2, descripcionServicio);
             ps.setDouble(3, precioServicio);
@@ -51,14 +52,14 @@ public class ServicesModel {
         }
     }
 
-    public static List<String> cargarServicio(String id){
+    // dado un ID, devuelve los datos del servicio correspondiente
+    public static List<String> cargarServicio(String id) {
         List<String> datosServicio = new ArrayList<>();
         String sql = "SELECT * FROM servicios WHERE id_servicio = ?";
 
         try (
                 Connection conexion = ConnectionModel.conectar();
-                PreparedStatement ps = conexion.prepareStatement(sql);
-        ) {
+                PreparedStatement ps = conexion.prepareStatement(sql);) {
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
 
@@ -76,10 +77,12 @@ public class ServicesModel {
         return datosServicio;
     }
 
-    public static int actualizarServicio(String id, String nombreServicio, String descripcionServicio, double precioServicio) {
+    // Dado un Id, actualiza los dados del servicio correspondinte
+    public static int actualizarServicio(String id, String nombreServicio, String descripcionServicio,
+            double precioServicio) {
         String sql = "UPDATE servicios SET nombre_servicio=?, descripcion_servicio=?, precio_servicio=? WHERE id_servicio=?";
         try (Connection conexion = ConnectionModel.conectar();
-             PreparedStatement ps = conexion.prepareStatement(sql)) {
+                PreparedStatement ps = conexion.prepareStatement(sql)) {
             ps.setString(1, nombreServicio);
             ps.setString(2, descripcionServicio);
             ps.setDouble(3, precioServicio);
@@ -91,10 +94,11 @@ public class ServicesModel {
         return 0;
     }
 
+    // Elimina el servicio de la BD
     public static int eliminarServicio(String id) {
         String sql = "UPDATE servicios SET visibilidad_servicio=0 WHERE id_servicio=?";
         try (Connection conexion = ConnectionModel.conectar();
-             PreparedStatement ps = conexion.prepareStatement(sql)) {
+                PreparedStatement ps = conexion.prepareStatement(sql)) {
             ps.setString(1, id);
             return ps.executeUpdate();
         } catch (SQLException e) {
