cf7f9258cc587c69c05b0be6754122deba74458a
Dani3lLopez <danlop112004@gmail.com>
2025-04-09 19:07:29 -0600

    validations

--- src/validations/FormatException.java
+++ src/validations/FormatException.java
@@ -1,5 +1,8 @@
 package src.validations;
 
+/**
+ * Validación de formato de entrada
+ */
 public class FormatException extends IllegalArgumentException {
     public FormatException(String msg) {
         super(msg);
